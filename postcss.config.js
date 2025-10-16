2df7a5b1e1010cb832f76a10e20fba510a09e7dd
Economiza Sul <economizasul@gemail.com>
2025-10-16 13:28:40 -0300

    Corrigindo ReferenceError: voltando para sintaxe ES Module no postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
 // Agora, usamos a sintaxe ESM (export default) que é compatível com "type": "module"
 export default {
-  plugins: [
-    // Usamos o require() dentro do array plugins (que é o formato que o PostCSS espera)
-    // Se isso falhar, a sintaxe alternativa deve ser usada (próximo passo, se necessário)
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
