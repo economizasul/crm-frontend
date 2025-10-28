1570f11d654068e15ab98345b6316741d5775193
Economiza Sul <economizasul@gemail.com>
2025-10-27 22:35:35 -0300

    Atualização do arquivo ppstcss.config.js e tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 export default {
   content: [
     "./index.html",
+    // Sintaxe simplificada e robusta para cobrir src/ e todas as subpastas
     "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
