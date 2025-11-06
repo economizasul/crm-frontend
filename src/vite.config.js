a03ab7697e237c49a49c6e321c45e9b16d0fc0aa
Economiza Sul <economizasul@gemail.com>
2025-11-06 14:11:21 -0300

    fix: base '/' - página branca resolvida

--- src/vite.config.js
+++ src/vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  resolve: {
-    alias: {
-      '@': '/', // <-- FORÇA O ALIAS PARA A RAIZ DO PROJETO DENTRO DO RENDER
-    },
-  },
+  base: '/',
 })
