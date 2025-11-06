bb8ebfdb8c5b6af75af3451a5a5ba9bc5234e26a
Economiza Sul <economizasul@gemail.com>
2025-11-06 14:50:35 -0300

    DEUS ETERNO: __dirname com fileURLToPath - Render 100000000000000000000%

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath } from 'node:url'
 
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
 export default defineConfig({
   plugins: [react()],
   base: '/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./', import.meta.url))
+      '@': __dirname // ← FORÇA O CAMINHO EXATO DA PASTA src NO RENDER
     }
   }
 })
