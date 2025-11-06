d38f6bf96437a17f06a4eee56255ea58ed8d34e0
Economiza Sul <economizasul@gemail.com>
2025-11-06 16:56:44 -0300

    DEUS: root src + outDir dist

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-// vite.config.js (NA RAIZ)
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  root: 'src',
   build: {
-    outDir: 'src/dist'
+    outDir: 'dist'
   }
 })
