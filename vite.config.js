da66efc92e82c91916cc6dc8dc74e5ebcd674f97
Economiza Sul <economizasul@gemail.com>
2025-11-06 14:30:06 -0300

    ULTIMATE WIN: vite.config.js na raiz + alias @ = src

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
@@ -7,7 +8,7 @@ export default defineConfig({
   base: '/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname)
-    }
-  }
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
